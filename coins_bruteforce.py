b47b16aa5f97cfc602153bf8e5c28e24318a4a44
JulixQuid <30515622+JulixQuid@users.noreply.github.com>
2020-08-01 11:35:35 -0500

    fix(cases) checked case when consecutive limits in xor case

--- coins_bruteforce.py
+++ coins_bruteforce.py
@@ -37,13 +37,14 @@ class coinMaster:
         print(self.conn.recvline())#There exists a coin minting ...
         print(self.conn.recvline())# empty line
         line=self.conn.recvline()
+        print(line)
         print(line.split()[-1])
         sol=self.binary_console(line.split()[-1])
-        self.conn.sendline(f'!{sol}')
+        self.conn.sendline(f'! {sol}')
         line = self.conn.recvline()
         print(line.split()[-1])
         sol = self.binary_console(line.split()[-1])
-        self.conn.sendline(f'!{sol}')
+        self.conn.sendline(f'! {sol}')
         print(self.conn.recvline())
         print(self.conn.recvline())
         print(self.conn.recvline())
@@ -56,75 +57,98 @@ class coinMaster:
         print(self.conn.recvline())#Go ahead, ask some queries
         lower = 0
         upper = int(size.decode()) - 1
+        solution=-1
         while (True):
             mid = (upper + lower) // 2
-            pad_low = (mid - lower) % 2
-            self.conn.sendline(f'{lower} {mid}')
-            print(f'{lower} {mid}')
+            pad_low = (mid - lower + 1) % 2
+            pad_high = (upper - mid + 1) % 2
+            if (upper - lower == 2):
+                print('difference is 2 so...')
+                self.conn.sendline(f'{lower} {lower+1}')
+                line3 = self.conn.recvline()
+                print(line3)
+                sleep(1)
+                num_3 = int(line1.split()[-1].decode())
+                print(f'[{lower}, {lower+1}]-> {num_3}')
+
+                self.conn.sendline(f'{upper-1} {upper}')
+                line4 = self.conn.recvline()
+                print(line4)
+                sleep(1)
+                num_4 = int(line1.split()[-1].decode())
+                print(f'[{upper-1}, {upper}]-> {num_4}')
+
+                if num_3 > num_4:
+                    solution =lower
+                    break
+                if num_3 == num_4:
+                    solution = lower+1
+                    break
+                if num_3 < num_4:
+                    solution = upper
+                    break
+            if (upper - lower == 1):
+                print('consecutive numbers ...')
+                self.conn.sendline(f'{lower-2} {lower}')
+                line3 = self.conn.recvline()
+                print(line3)
+                num_3 = int(line1.split()[-1].decode())
+                print(f'[{lower-2}, {lower}]-> {num_3}')
+
+                self.conn.sendline(f'{upper} {upper+2}')
+                line4 = self.conn.recvline()
+                print(line4)
+                num_4 = int(line1.split()[-1].decode())
+                print(f'[{upper}, {upper+2}]-> {num_4}')
+
+                if num_3 < num_4:
+                    print()
+                    solution = upper
+                    break
+                if num_4 > num_3:
+                    solution= lower
+                    break
+                if num_3 == num_4:
+                    solution =-1
+                    print('you f***ed it up man !')
+
+
+            print('still a long way to go...')
+            self.conn.sendline(f'{lower} {mid+pad_low}')
             line1 = self.conn.recvline()
             print(line1)
             sleep(1)
             num_1 = int(line1.split()[-1].decode())
-            print(num_1)
-            pad_high = (upper - mid) % 2
-            print(f'{lower} {mid}')
-            self.conn.sendline(f'{lower} {mid}')
+            print(f'[{lower}, {mid+pad_low}]-> {num_1}')
+
+            self.conn.sendline(f'{mid-pad_high} {upper}')
             line2 = self.conn.recvline()
             print(line2)
             sleep(1)
             num_2 = int((line2.split()[-1]).decode())
-            print(num_2)
+            print(f'[{mid-pad_high}, {upper}]-> {num_2}')
+
             print(f'{num_1}->{lower},{mid + pad_low},<<{mid}>>,{mid - pad_high},{upper}<-{num_2}')
-            if (upper - lower == 2):
-                self.conn.sendline(f'{lower} {mid}')
-                print(f'{lower} {mid}')
-                line3 = self.conn.recvline()
-                print(line3)
-                num_3 = int(line1.split()[-1].decode())
-                print(num_3)
+            print('comparing cases...')
 
-                self.conn.sendline(f'{mid} {upper}')
-                print(f'{mid} {upper-pad_high}')
-                line4 = self.conn.recvline()
-                print(line4)
-                num_4 = int(line1.split()[-1].decode())
-                print(num_4)
 
-                if num_3 > num_4:
-                    return lower
-                if num_3 == num_4:
-                    return mid
-                if num_3 < num_4:
-                    return upper
-            if (upper - lower == 1):
-                self.conn.sendline(f'{lower-1} {lower}')
-                line3 = self.conn.recvline()
-                print(line3)
-                num_3 = int(line1.split()[-1].decode())
-                print(num_3)
-
-                self.conn.sendline(f'{upper} {upper+1}')
-                line4 = self.conn.recvline()
-                print(line4)
-                num_4 = int(line1.split()[-1].decode())
-                print(num_4)
-
-                if num_3 < num_4:
-                    return upper
-                if num_4 > num_3:
-                    return lower
-                if num_3 == num_4:
-                    return -1
             if (num_1 == 0):
+                print('upper half')
                 lower = mid - pad_high
             if (0 == num_2):
+                print('lower half')
                 upper = mid + pad_low
             if (num_1 == num_2):
+                print('damn bro... that escalated quickly!!!')
                 lower = mid - pad_high
                 upper = mid + pad_low
             if(lower==upper):
-                return lower
-            print(f'{lower},{(lower + upper) // 2},{upper}')
+                solution = lower
+                break
+
+            print(f'new range: {lower},{(lower + upper) // 2},{upper}')
+        print(f'solution {solution}')
+        return solution
 
     def binary_man(numbers):
         lower=0
