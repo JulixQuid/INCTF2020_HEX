213783dd6bc1f348520607a9352a78a42ce4891a
JulixQuid <30515622+JulixQuid@users.noreply.github.com>
2020-08-01 02:17:12 -0500

    fix(bug): fixed return in decrypt

--- coins_bruteforce.py
+++ coins_bruteforce.py
@@ -1,12 +1,11 @@
-
 import hashlib
 import random
 from pwn import *
-
+from time import sleep
+import numpy as np
 class coinMaster:
     def __init__(self):
         self.conn = remote('34.74.30.191', 1337)
-
 
     def find_key(self,coin_suffix,encrypted):
         i=0
@@ -25,27 +24,35 @@ class coinMaster:
                         intento = hashlib.sha256(bruteforcing).hexdigest()
                         if (intento == encrypted):
                             print('cracked!')
-                            return intento[0:4]
-
-
+                            return bruteforcing.decode()
     def interaction(self):
-        line=self.conn.recvline()
-        encrypted=line.split()[2].decode()
-        sufix=line.split()[0].decode().split('+')[1].split(')')[0]
-        key=self.find_key(sufix,encrypted)
+        line = self.conn.recvline().decode("utf-8")
+        sufix = line[12:28]
+        encrypted = line[33:-1]
+        print(sufix, encrypted)
+        key = self.find_key(sufix, encrypted)
         print(key)
-        self.conn.sendline(key)
-        try:
-            print(self.conn.recvline())
-        except:
-            self.conn.sendline(key)
-        print('xxxxxxxx')
+        print(self.conn.recvline())
+        self.conn.sendline(key[0:4])
+        print(self.conn.recvline())
+        print(self.conn.recvline())
+        line=self.conn.recvline()
+        self.binary_man(line.split()[-1])
+
+    def binary_man(self,size):
+        print(size)
 
 
 
+
+    def jumpstart(self,line):
+        encrypted = line.split()[2].decode()
+        sufix = line.split()[0].decode().split('+')[1].split(')')[0]
+        key = self.find_key(sufix, encrypted)
+        print(key)
 
 if __name__ == "__main__":
-    rod=coinMaster()
+    rod = coinMaster()
     rod.interaction()
 
 
