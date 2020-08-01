ecdc3cf14258ed7390c994c51e400426e9c6e8a6
JulixQuid <30515622+JulixQuid@users.noreply.github.com>
2020-08-01 09:21:41 -0500

    feat(function): added xor binary search function

--- coins_bruteforce.py
+++ coins_bruteforce.py
@@ -37,19 +37,104 @@ class coinMaster:
         print(self.conn.recvline())
         print(self.conn.recvline())
         line=self.conn.recvline()
-        self.binary_man(line.split()[-1])
+        print(line.split()[-1])
+        sol1=self.binary_console(line.split()[-1])
+        self.conn.sendline(str(sol1))
+        line = self.conn.recvline()
+        print(line.split()[-1])
+        sol1 = self.binary_console(line.split()[-1])
+        self.conn.sendline(str(sol1))
 
-    def binary_man(self,size):
-        print(size)
+    def binary_console(self,size):
+        lower = 0
+        upper = int(size.decode()) - 1
+        while (True):
+            mid = (upper + lower) // 2
+            pad_low = (mid - lower) % 2
+            self.conn.sendline(f'{lower} {mid}')
+            line1 = self.conn.recvline()
+            print(line1)
+            sleep(1)
+            num_1 = int(line1.split()[-1].decode())
+            print(num_1)
+            pad_high = (upper - mid) % 2
+            self.conn.sendline(f'{lower} {mid}')
+            line2 = self.conn.recvline()
+            print(line2)
+            sleep(1)
+            num_2 = int((line2.split()[-1]).decode())
+            print(num_2)
+            print(f'{num_1}->{lower},{mid + pad_low},<<{mid}>>,{mid - pad_high},{upper}<-{num_2}')
+            if (upper - lower == 2):
+                num_3 = numbers[lower] ^ numbers[mid]
+                num_4 = numbers[mid] ^ numbers[upper]
+                if num_3 > num_4:
+                    return lower
+                if num_3 == num_4:
+                    return mid
+                if num_3 < num_4:
+                    return upper
+            if (upper - lower == 1):
+                num_3 = numbers[lower] ^ numbers[lower - 1]
+                num_4 = numbers[upper] ^ numbers[upper + 1]
+                if num_3 < num_4:
+                    return upper
+                if num_4 > num_3:
+                    return lower
+                if num_3 == num_4:
+                    return -1
+            if (num_1 == 0):
+                lower = mid - pad_high
+            if (0 == num_2):
+                upper = mid + pad_low
+            if (num_1 == num_2):
+                lower = mid - pad_high
+                upper = mid - pad_low
+            print(f'{lower},{(lower + upper) // 2},{upper}')
+
+    def binary_man(numbers):
+        lower=0
+        upper=len(numbers)-1
+        while(True):
+            mid=(upper+lower)//2
+            pad_low=(mid-lower)%2
+            num_1=reduce(lambda i, j: i ^ j, numbers[lower:mid+pad_low])
+            print(num_1)
+            pad_high=(upper-mid)%2
+            num_2 = reduce(lambda i, j: i ^ j, numbers[mid-pad_high:upper])
+            print(num_2)
+            print(f'{num_1}->{lower},{mid+pad_low},<<{mid}>>,{mid - pad_high},{upper}<-{num_2}')
+            if(upper-lower==2):
+                num_3 = numbers[lower] ^ numbers[mid]
+                num_4 = numbers[mid] ^ numbers[upper]
+                if num_3>num_4:
+                    return lower
+                if num_3==num_4:
+                    return mid
+                if num_3<num_4:
+                    return upper
+            if(upper-lower==1):
+                num_3 = numbers[lower] ^ numbers[lower-1]
+                num_4 = numbers[upper] ^ numbers[upper+1]
+                if num_3 < num_4:
+                    return upper
+                if num_4 > num_3:
+                    return lower
+                if  num_3 == num_4:
+                    return -1
+            if(num_1==0):
+                lower=mid-pad_high
+            if(0==num_2):
+                upper=mid+pad_low
+            if(num_1==num_2):
+                lower=mid-pad_high
+                upper=mid-pad_low
+            print(f'{lower},{(lower+upper)//2},{upper}')
+            jaja=input()
 
 
 
 
-    def jumpstart(self,line):
-        encrypted = line.split()[2].decode()
-        sufix = line.split()[0].decode().split('+')[1].split(')')[0]
-        key = self.find_key(sufix, encrypted)
-        print(key)
 
 if __name__ == "__main__":
     rod = coinMaster()
